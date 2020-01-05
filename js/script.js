9e22a538e2cb497ce25c9f30deb044a339e1d947
Qusai Fares <qusaifares@gmail.com>
2020-01-05 16:05:03 -0500

    cleaned code and removed unneeded variables

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
 // Target DOM elements
 const choiceBoxes = document.querySelectorAll('.choice');
 const questionText = document.querySelector('.question');
-const choiceOne = document.querySelector('.choice-1');
-const choiceTwo = document.querySelector('.choice-2');
-const choiceThree = document.querySelector('.choice-3');
-const choiceFour = document.querySelector('.choice-4');
-// create variables
-let questionIndex = 0;
-let currentScore = 0;
+const scoreboard = document.querySelector('.scoreboard');
 
-// create class for questions
+// create class for questions and quiz
 class Trivia {
   constructor(questions) {
     this.questions = questions;
     this.questionIndex = 0;
     this.score = 0;
+    this.numberOfQuestions = questions.length;
   }
   showQuestion() {
-    questionText.innerText = this.questions[questionIndex].question;
-    choiceOne.innerText = this.questions[questionIndex].choices[0];
-    choiceTwo.innerText = this.questions[questionIndex].choices[1];
-    choiceThree.innerText = this.questions[questionIndex].choices[2];
-    choiceFour.innerText = this.questions[questionIndex].choices[3];
+    questionText.innerText = this.questions[this.questionIndex].question;
+    for (let i = 0; i < choiceBoxes.length; i++) {
+      choiceBoxes[i].innerText = this.questions[this.questionIndex].choices[i];
+    }
   }
   makeChoice(e) {
-    return e.target.innerText === this.question.answer;
+    let guess = e.target.innerText;
+    this.changeScore(guess);
+    if (this.checkGameEnd()) {
+      console.log(this.checkGameEnd());
+    } else {
+      this.nextQuestion();
+    }
+  }
+  changeScore(guessedAnswer) {
+    if (guessedAnswer === this.questions[this.questionIndex].answer)
+      this.score++;
+    scoreboard.innerText = `Score: ${this.score}/${this.numberOfQuestions}`;
   }
   nextQuestion() {
     this.questionIndex++;
+    this.showQuestion();
+  }
+  checkGameEnd() {
+    return this.questionIndex >= this.numberOfQuestions - 1;
   }
 }
 
@@ -41,20 +50,29 @@ class Question {
 
 soccerQuestions = [
   new Question(
-    'Who won the last world cup?',
-    ['france', 'argentina', 'brazil', 'spain'],
-    'france'
+    'Which team won the 2018 FIFA World Cup?',
+    ['Brazil', 'Germany', 'France', 'Argentina'],
+    'France'
   ),
   new Question(
-    'Who won the 2014 world cup?',
-    ['germany', 'argentina', 'brazil', 'netherlands'],
-    'germany'
+    'How many panels is a classic soccer ball made of?',
+    ['8', '16', '32', '64'],
+    '32'
+  ),
+  new Question(
+    'Who is the only player to win player of the year 6 times?',
+    ['Pele', 'Cristiano Ronaldo', 'Diego Maradona', 'Lionel Messi'],
+    'Lionel Messi'
   )
 ];
 
 let soccerTrivia = new Trivia(soccerQuestions);
 
 soccerTrivia.showQuestion();
+
+choiceBoxes.forEach(choice => {
+  choice.addEventListener('click', e => soccerTrivia.makeChoice(e));
+});
 
 // when choice is clicked, compare to answer
 // if answer is correct, add 1 to score
