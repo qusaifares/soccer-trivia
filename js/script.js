e3f71668e00c55b951788ffae2dcac50aef4075e
Qusai Fares <qusaifares@gmail.com>
2020-01-05 16:05:03 -0500

    added a class for trivia and some logic

--- js/script.js
+++ js/script.js
@@ -7,9 +7,29 @@ const choiceThree = document.querySelector('.choice-3');
 const choiceFour = document.querySelector('.choice-4');
 // create variables
 let questionIndex = 0;
-let score = 0;
+let currentScore = 0;
 
 // create class for questions
+class Trivia {
+  constructor(questions) {
+    this.questions = questions;
+    this.questionIndex = 0;
+    this.score = 0;
+  }
+  showQuestion() {
+    questionText.innerText = this.questions[questionIndex].question;
+    choiceOne.innerText = this.questions[questionIndex].choices[0];
+    choiceTwo.innerText = this.questions[questionIndex].choices[1];
+    choiceThree.innerText = this.questions[questionIndex].choices[2];
+    choiceFour.innerText = this.questions[questionIndex].choices[3];
+  }
+  makeChoice(e) {
+    return e.target.innerText === this.question.answer;
+  }
+  nextQuestion() {
+    this.questionIndex++;
+  }
+}
 
 class Question {
   constructor(question, choices, answer) {
@@ -19,7 +39,7 @@ class Question {
   }
 }
 
-questions = [
+soccerQuestions = [
   new Question(
     'Who won the last world cup?',
     ['france', 'argentina', 'brazil', 'spain'],
@@ -32,10 +52,11 @@ questions = [
   )
 ];
 
-let pageSetup = () => {
-  questionText.innerText = questions[questionIndex].question;
-  choiceOne.innerText = questions[questionIndex].choices[0];
-  choiceTwo.innerText = questions[questionIndex].choices[1];
-  choiceThree.innerText = questions[questionIndex].choices[2];
-  choiceFour.innerText = questions[questionIndex].choices[3];
-};
+let soccerTrivia = new Trivia(soccerQuestions);
+
+soccerTrivia.showQuestion();
+
+// when choice is clicked, compare to answer
+// if answer is correct, add 1 to score
+// change index to next question
+// show new question
